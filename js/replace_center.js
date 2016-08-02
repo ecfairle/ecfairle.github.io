ad9668357c854cd3eca65d91c9a9e04446dc970b
Eugene Fairley <ecfairley@ucdavis.edu>
2016-08-01 18:39:16 -0700

    stuff I like

--- js/replace_center.js
+++ js/replace_center.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
 	$buttons = $('#SIFT,#CAIR');
 	$buttons.on('click', function(event) {
 		event.preventDefault();
-		link = $(this).attr('id') + '.html';
+		id = $(this).attr('id');
+		if(id == "HOME"){
+			$('#extra').html('');
+		}
+		link = id + '.html';
 		$('#extra').load(link);
 	});
 });
