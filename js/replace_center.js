2d080fa04d9b4f15e75a0349f45f90db33e07613
Eugene Fairley <ecfairley@ucdavis.edu>
2016-08-02 20:45:10 -0700

    click picture

--- js/replace_center.js
+++ js/replace_center.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-	$buttons = $('#SIFT,#CAIR');
+	$buttons = $('#SIFT,#CAIR,#home');
 	$buttons.on('click', function(event) {
 		event.preventDefault();
 		id = $(this).attr('id');
-		if(id == "HOME"){
+		if(id == "home"){
 			$('#extra').html('');
+			return false;
 		}
 		link = id + '.html';
 		$('#extra').load(link);
